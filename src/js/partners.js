f930bad9dc40b85e7f89ca2ab2092f825ae081ee
work-ilya-web <ilya.balyasov48@gmail.com>
2021-09-13 12:21:55 +0300

    fix

---
+++ src/js/partners.js
@@ -0,0 +1,10 @@
+$('.partners__slick').slick({
+	slidesToShow: 6,
+	slidesToScroll: 1,
+	arrows: false,
+	dots: false,
+	infinite: true,
+	speed: 1000,
+	autoplay: false,
+	autoplaySpeed: 2000,
+});
---
+++ src/js/reviews.js
@@ -0,0 +1,11 @@
+$('.reviews__slick').slick({
+	slidesToShow: 2,
+	slidesToScroll: 1,
+	arrows: false,
+	dots: false,
+	infinite: true,
+	speed: 1000,
+	variableWidth: true,
+	autoplay: false,
+	autoplaySpeed: 2000,
+});
